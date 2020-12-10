cdeb93a6a9e8c1446c3d0e660835570d2d22ebab
fraser jamieson <fraserjamieson.93@gmail.com>
2020-12-10 20:29:09 +0000

    reset form function added.  new item entries showing without inputs.

--- js/app.js
+++ js/app.js
@@ -5,20 +5,36 @@ document.addEventListener('DOMContentLoaded', () => {
   const inputForm = document.querySelector('#new-item-form');     // GRABBING WHOLE FORM ITEM
   inputForm.addEventListener('submit', handleFormSubmission);
 
-});
+  const resetForm = document.querySelector('#new-item-form');     // Event listener after saving entry > reset input fields
+  resetForm.addEventListener('submit', handleResettingForm);
 
+  const insertNewEntry = document.querySelector('#new-item-form');     
+  insertNewEntry.addEventListener('submit', handleNewListEntry);
+
+});
 
   const handleFormSubmission = function (event) {
 
     event.preventDefault();
-    console.log(event.target.title.value);
-
     const resultItems = document.querySelector('#reading-list');  
     // WE SHOULD BE QUERYING/SELECTING THE NEWLY CREATED UL>LI ITEM FOR APPENDING CONTENT 
-    resultItems.textContent = (`Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`);
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value;
+    resultItems.textContent = (`Title: ${title} Author: ${author} Category: ${category}`);
 };
 
-    // I NEED TO FIND A WAY TO ATTACH ID FOR BUTTON ELEMENT TO BE REFERRED TO IN FORM SUBMISSION FUNCTION 
+  const handleResettingForm = function () {
+    const resetForm = document.querySelector('#new-item-form');  
+    resetForm.reset();
+  }
 
+  const handleNewListEntry = function (event) {
+    const newEntry = document.createElement('li');
+    const newContent = document.createTextNode (`Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`);
+    newEntry.appendChild(newContent);
+    const newItem = document.querySelector('li');
+    document.body.insertBefore(newEntry, newItem);
+  };
 
-  // when button clicked, create new event listener for submission form and also element for holding info in list item
+    // MY FORM IS RESETTING THE ORIGINAL INSERT TO THE NEW FORM INPUT
