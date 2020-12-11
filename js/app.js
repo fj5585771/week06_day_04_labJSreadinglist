c0e15343c00e3e78598aa75e7bbef6c8c397d412
fraser jamieson <fraserjamieson.93@gmail.com>
2020-12-11 09:56:20 +0000

    finished MVP

--- js/app.js
+++ js/app.js
@@ -8,8 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const resetForm = document.querySelector('#new-item-form');     // Event listener after saving entry > reset input fields
   resetForm.addEventListener('submit', handleResettingForm);
 
-  const insertNewEntry = document.querySelector('#new-item-form');     
-  insertNewEntry.addEventListener('submit', handleNewListEntry);
 
 });
 
@@ -18,23 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
     event.preventDefault();
     const resultItems = document.querySelector('#reading-list');  
     // WE SHOULD BE QUERYING/SELECTING THE NEWLY CREATED UL>LI ITEM FOR APPENDING CONTENT 
+    const newEntry = document.createElement('li');
+    resultItems.appendChild(newEntry);
     const title = event.target.title.value;
     const author = event.target.author.value;
     const category = event.target.category.value;
-    resultItems.textContent = (`Title: ${title} Author: ${author} Category: ${category}`);
+    newEntry.textContent = (`Title: ${title} Author: ${author} Category: ${category}`);
 };
 
   const handleResettingForm = function () {
     const resetForm = document.querySelector('#new-item-form');  
     resetForm.reset();
   }
-
-  const handleNewListEntry = function (event) {
-    const newEntry = document.createElement('li');
-    const newContent = document.createTextNode (`Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`);
-    newEntry.appendChild(newContent);
-    const newItem = document.querySelector('li');
-    document.body.insertBefore(newEntry, newItem);
-  };
 
     // MY FORM IS RESETTING THE ORIGINAL INSERT TO THE NEW FORM INPUT
